096af3eee3644c9c1ab41cb672c27cc61e751b90
Cynthia Silva <cynthias2014@cynthias-macbook-air.local>
2017-07-17 22:03:47 -0400

    Initial Commit

---
+++ DestroyTheWorld2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld2
+//
+//  Created by Cynthia Silva on 7/17/17.
+//  Copyright © 2017 Cynthia Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
